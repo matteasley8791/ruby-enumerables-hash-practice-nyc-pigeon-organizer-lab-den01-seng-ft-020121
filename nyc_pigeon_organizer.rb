092720aaf9b069bc4bc0d495ee9a972bd35da74b
Matthew Easley <matteasley8791@gmail.com>
2021-01-25 16:39:29 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,4 +1,16 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
   final_results = data.each_with_object({}) do |(key, value), final_array|
+    value.each do |inner_keys, names|
+      names.each do |name|
+        if !final_array[name]
+          final_array[name] = {}
+        end
+        if !final_array[name][key]
+          !final_array[name][key] = []
+        end
+        final_array[name][key].push(inner_keys.to_s)
+      end
+    end
+  end
 end
